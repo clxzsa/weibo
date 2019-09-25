9404ad6f95b2071d898865f7ea9a5f714a9d6dc9
Zuo XiaoZuo <zsait168@126.com>
2019-09-25 02:15:01 +0000

    用户注册页面

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,12 @@
 |
 */
 
+//主页
 Route::get('/', 'StaticPagesController@home')->name('home');
+//帮助
 Route::get('/help', 'StaticPagesController@help')->name('help');
+//关于
 Route::get('/about', 'StaticPagesController@about')->name('about');
+
+//登录
+Route::get('signup', 'UsersController@create')->name('signup');
