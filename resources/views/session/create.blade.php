2266e800df120b7705c64e03b42999816914dce3
Zuo XiaoZuo <zsait168@126.com>
2019-09-26 07:22:30 +0000

    登录时记住我

--- resources/views/session/create.blade.php
+++ resources/views/session/create.blade.php
@@ -2,15 +2,15 @@
 @section('title', '登录')
 
 @section('content')
-  <div class="offset-md-2 col-md-8">
-    <div class="card ">
-      <div class="card-header">
-        <h5>登录</h5>
-      </div>
-      <div class="card-body">
-        @include('shared._errors')
+<div class="offset-md-2 col-md-8">
+  <div class="card ">
+    <div class="card-header">
+      <h5>登录</h5>
+    </div>
+    <div class="card-body">
+      @include('shared._errors')
 
-        <form method="POST" action="{{ route('login') }}">
+      <form method="POST" action="{{ route('login') }}">
           {{ csrf_field() }}
 
           <div class="form-group">
@@ -23,13 +23,20 @@
             <input type="password" name="password" class="form-control" value="{{ old('password') }}">
           </div>
 
+          <div class="form-group">
+            <div class="form-check">
+              <input type="checkbox" class="form-check-input" name="remember" id="exampleCheck1">
+              <label class="form-check-label" for="exampleCheck1">记住我</label>
+            </div>
+          </div>
+
           <button type="submit" class="btn btn-primary">登录</button>
-        </form>
+      </form>
 
-        <hr>
+      <hr>
 
-        <p>还没账号？<a href="{{ route('signup') }}">现在注册！</a></p>
-      </div>
+      <p>还没账号？<a href="{{ route('signup') }}">现在注册！</a></p>
     </div>
   </div>
+</div>
 @stop
