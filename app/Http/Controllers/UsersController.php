1a841b74ccbab6eed67d1c18766e067353f805b3
Zuo XiaoZuo <zsait168@126.com>
2019-09-26 05:56:55 +0000

    完成用户注册功能

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\User;
+use function React\Promise\reduce;
 
 class UsersController extends Controller
 {
@@ -33,7 +34,21 @@ class UsersController extends Controller
             'email'    => 'required|email|unique:users|max:255',
             'password' => 'required|confirmed|min:6',
         ]);
-        return;
+
+        //获取用户输入的所有数据
+        // $data = $request->all();
+
+        $user = User::create([
+            'name'     => $request->name,
+            'email'    => $request->email,
+            'password' => bcrypt($request->password),
+        ]);
+
+        // 我们想存入一条缓存的数据，让它只在下一次的请求内有效时，则可以使用 flash 方法。flash 方法接收两个参数，第一个为会话的键，第二个为会话的值，我们可以通过下面这行代码的为会话赋值。
+        session()->flash('success', '欢迎，您将在这里开启一段新的旅程~');
+
+        return redirect()->route('users.show', [$user]);
+
     }
 
 }
--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -1,20 +1,18 @@
 <!DOCTYPE html>
 <html>
 <head>
-  <title>@yield('title', 'Weibo App') - 2021 SJTU</title>
+  <title>@yield('title', 'Weibo App') - 2021 - SJTU</title>
   <link rel="stylesheet" href="{{ mix('css/app.css') }}">
 </head>
 
 <body>
-
 @include('layouts._header')
 
 <div class="container">
   <div class="offset-md-1 col-md-10">
+    @include('shared._messages')
     @yield('content')
-
     @include('layouts._footer')
-
   </div>
 </div>
 </body>
